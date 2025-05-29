748878897b353cc2e662a63b9a13941ef58b64a7
Frank <frank@nx01.de>
2025-05-29 20:06:31 +0200

    Dockerfile testing

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -20,38 +20,38 @@ mkdir -p /var/spool/gosa
 mkdir -p /etc/gosa
 
 # Erstelle GOsa Konfiguration basierend auf Umgebungsvariablen
-cat > /etc/gosa/gosa.conf << EOF
-<?xml version="1.0"?>
-<conf>
-  <main>
-    <location name="default">
-      <referral uri="${LDAP_SERVER}/${LDAP_BASE_DN}"
-                admin="${LDAP_ADMIN_DN}"
-                password="${LDAP_ADMIN_PASSWORD}" />
-    </location>
-
-    <globals>
-      <sessionLifetime>7200</sessionLifetime>
-      <passwordDefaultHash>ssha</passwordDefaultHash>
-      <passwordMinLength>8</passwordMinLength>
-      <logging>true</logging>
-      <logFile>/dev/stdout</logFile>
-      <cacheDirectory>/var/cache/gosa</cacheDirectory>
-      <spoolDirectory>/var/spool/gosa</spoolDirectory>
-      <language>de</language>
-      <theme>default</theme>
-      <debugLevel>0</debugLevel>
-    </globals>
-  </main>
-</conf>
-EOF
-
-# Kopiere Konfiguration nach GOsa-Verzeichnis
-cp /etc/gosa/gosa.conf /var/www/gosa/gosa.conf
-
-# Setze Berechtigungen
-chmod 640 /var/www/gosa/gosa.conf
-
+#cat > /etc/gosa/gosa.conf << EOF
+#<?xml version="1.0"?>
+#<conf>
+#  <main>
+#    <location name="default">
+#      <referral uri="${LDAP_SERVER}/${LDAP_BASE_DN}"
+#                admin="${LDAP_ADMIN_DN}"
+#                password="${LDAP_ADMIN_PASSWORD}" />
+#    </location>
+#
+#    <globals>
+#      <sessionLifetime>7200</sessionLifetime>
+#      <passwordDefaultHash>ssha</passwordDefaultHash>
+#      <passwordMinLength>8</passwordMinLength>
+#      <logging>true</logging>
+#      <logFile>/dev/stdout</logFile>
+#      <cacheDirectory>/var/cache/gosa</cacheDirectory>
+#      <spoolDirectory>/var/spool/gosa</spoolDirectory>
+#      <language>de</language>
+#      <theme>default</theme>
+#      <debugLevel>0</debugLevel>
+#    </globals>
+#  </main>
+#</conf>
+#EOF
+#
+## Kopiere Konfiguration nach GOsa-Verzeichnis
+#cp /etc/gosa/gosa.conf /var/www/gosa/gosa.conf
+#
+## Setze Berechtigungen
+#chmod 640 /var/www/gosa/gosa.conf
+#
 echo "GOsa² configuration created successfully"
 echo "Starting PHP-FPM..."
 
